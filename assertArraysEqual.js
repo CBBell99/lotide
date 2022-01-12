bade1500e82574bc56833a7a319748cf9f9191f5
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-11 18:03:39 -0800

    initial commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(arr1, arr2) {
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
+assertArraysEqual([1, 2], [1, 2]);
+assertArraysEqual([1, 2], [3, 4]);
