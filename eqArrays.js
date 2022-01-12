400ee6359ff9bfda7a557fead4b822322c84e84c
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-11 17:47:31 -0800

    eslint fixes

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
--- tail.js
+++ tail.js
@@ -13,3 +13,5 @@ const tail = function(array) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words[0], 'Lighthouse');
+assertEqual(words[1], 'Labs');
