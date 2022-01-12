4e61941fa64acd9a4dc3c05afa820faad54f769e
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-12 15:41:58 -0800

    initial commit

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  let result = {};
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      if (result[item]) {
+        result[item]++;
+      } else {
+        result[item] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
