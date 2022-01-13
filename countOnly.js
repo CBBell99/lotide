27c26189ef93423e32ef1af7e797f2078d796fcb
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-13 07:16:44 -0800

    fixed lint errors`

--- countOnly.js
+++ countOnly.js
@@ -9,8 +9,8 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-  let result = {};
-  for (let item of allItems) {
+  const result = {};
+  for (const item of allItems) {
     if (itemsToCount[item]) {
       if (result[item]) {
         result[item]++;
