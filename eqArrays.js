af821edf621da2d3516f190da29d22a1814f6947
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-13 15:54:08 -0800

    fixed various errors

--- eqArrays.js
+++ eqArrays.js
@@ -7,7 +7,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
--- letterPosition.js
+++ letterPosition.js
@@ -18,7 +18,7 @@ const assertArraysEqual = function(arr1, arr2) {
 
 
 const letterPositions = function(sentence) {
-  const results = {};
+  let results = {};
   for (let i = 0; i < sentence.length; i++) {
     if ((sentence[i] !== " ")) {
       if (results[sentence[i]]) {
@@ -30,4 +30,5 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
-assertArraysEqual(letterPositions('lighthouse in the house').e[1]);
+console.log(letterPositions('lighthouse in the house'));
+assertArraysEqual(letterPositions('hello'), { h: [0], e: [1], l: [2, 3], o: [4] });
