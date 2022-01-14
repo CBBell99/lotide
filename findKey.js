3183ff6e1ec5eb601312a979edf9c2ca26d7b151
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-13 18:57:10 -0800

    initial commit

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = (object, callback) => {
+  for (let key of Object.keys(object)) {
+    if (callback(object[key])) return key
+  }
+}
+
+assertEqual('noma', findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
+
