a71916bf2d460662c8dd8ec6496c542d2bb57f11
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-17 15:56:13 -0800

    incorporated mocha & chai for test files

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,17 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "hello");
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "hello");
+
+const assert = require('chai').assert;
+const head = require('../head')
+
+describe('#head returns the first item of an array', () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1)
+  });
+  it('returns "6" for ["5"]', () => {
+    assert.strictEqual(head(['5']), '5')
+  });
+});
