c1d53c5b7e28215fe465e4f4bc5072a150feba81
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-17 14:46:54 -0800

    exported eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
@@ -18,7 +13,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, '2', 3], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 'WWDD'], [1, 2, 3]), true);
-
+module.exports = eqArrays;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual('Jimmy jab Games', 'Jimmy Jab Games');
+assertEqual('recuérdame', 'recuerdame');
+assertEqual(2099, 2099);
+assertEqual(1985, 1985);
