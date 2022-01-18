504dd98c8b9cd22ac0dd5ba09633c2b09ed0876c
Chris Bell <83792902+CBBell99@users.noreply.github.com>
2022-01-17 16:37:53 -0800

    Update headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -11,7 +11,7 @@ describe('#head returns the first item of an array', () => {
   it('returns 1 for [1, 2, 3]', () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-  it('returns "6" for ["5"]', () => {
+  it('returns "5" for ["5"]', () => {
     assert.strictEqual(head(['5']), '5');
   });
 });
