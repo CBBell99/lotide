2b2f9aa34cf6922c38c67d4fe15b0662ab69d8ab
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-17 16:12:23 -0800

    fixed linting errors

--- test/headTest.js
+++ test/headTest.js
@@ -5,13 +5,13 @@
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "hello");
 
 const assert = require('chai').assert;
-const head = require('../head')
+const head = require('../head');
 
 describe('#head returns the first item of an array', () => {
   it('returns 1 for [1, 2, 3]', () => {
-    assert.strictEqual(head([1, 2, 3]), 1)
+    assert.strictEqual(head([1, 2, 3]), 1);
   });
   it('returns "6" for ["5"]', () => {
-    assert.strictEqual(head(['5']), '5')
+    assert.strictEqual(head(['5']), '5');
   });
 });
