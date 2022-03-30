4dc27a269483c83dc9b1d55a2a933e715c50fdea
CBBell99 <chrisbeckertonbell@gmail.com>
2022-03-30 11:26:28 -0700

    more symbols

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,9 +2,9 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
+    console.log(`✅✅Assertion Passed: ${arr1} === ${arr2}`);
   } else {
-    console.log(`🛑Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`🛑🛑Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
 
